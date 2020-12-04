d3b5c0592945ee167955fe06634b7255879f126b
AngelicaCorrales <angelicacorrales2002@gmail.com>
2020-12-03 23:18:01 -0500

    javadoc Price and Level interfaces modified

--- src/model/Level.java
+++ src/model/Level.java
@@ -1,4 +1,10 @@
 package model;
 public interface Level{
+	/**
+	*<b>name:</b> calculateLevel <br>
+	*This method calculates the level of a player or a head coach<br>
+	*<b>post:</b> the level of the employee has been calculated <br>
+	*@return an <code>integer</code> specifying the level of the employee
+	*/
 	public int calculateLevel();
 }
--- src/model/Price.java
+++ src/model/Price.java
@@ -1,4 +1,10 @@
 package model;
 public interface Price{
+	/**
+	*<b>name:</b> calculatePrice <br>
+	*This method calculates the market price of a player or a head coach<br>
+	*<b>post:</b> the market price of the employee has been calculated <br>
+	*@return a <code>double</code> specifying the market price of the employee
+	*/
 	public double calculatePrice();
 }
